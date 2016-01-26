336a29888734c3a1d0ffed43d4c174326a79378d
evanwchen <evanwchen@gmail.com>
2016-01-25 16:23:29 -0800

    Create postMessage feature

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -26,5 +26,30 @@ var retrieveData = function() {
 
 // setInterval(retrieveData, 2000);
 
+
+var postMessage = function(message) {
+  $.ajax({
+    // This is the url you should use to communicate with the parse API server.
+    url: 'https://api.parse.com/1/classes/chatterbox',
+    type: 'POST',
+    data: JSON.stringify(message),
+    contentType: 'application/json',
+    success: function (data) {
+      console.log("success");
+      console.log("data:", data);
+      console.log("message:", message);
+    },
+    error: function (data) {
+      // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to send message');
+    }
+  });
+};
+
+postMessage({username:'eSquared', text:'eSquared test 2',roomname:'4chan'});
+
 retrieveData();
 
+// $.post("https://api.parse.com/1/classes/chatterbox", function(data) {
+//   $("#chats").html(data);
+// });
